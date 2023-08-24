390beae423e3a7c7d0a39bc858662a4245b19053
Areum <paloma.mmendonca@hotmail.com>
2023-08-24 10:34:08 -0300

    Primeiras aulas de Python

---
+++ Python/brincando-com-python.py
@@ -0,0 +1,12 @@
+print(11+40)
+nome = "Lili"
+idade = 50
+nome, idade = "Catarina", 13
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "AM", "RS"]
+
+print(BRAZILIAN_STATES)
